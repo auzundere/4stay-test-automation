ca2c028eca0fdb92a8daabe4cf72c439d361d1aa
auzundere <admin@192.168.1.2>
2018-03-14 17:42:44 -0500

    Added WebElemennt to SignUpPage class By Uzundere

--- src/test/java/com/fourstay/pages/SignUpPage.java
+++ src/test/java/com/fourstay/pages/SignUpPage.java
@@ -1,6 +1,8 @@
 package com.fourstay.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class SignUpPage {
@@ -11,4 +13,11 @@ public class SignUpPage {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
+	
+	@FindBy(xpath="//a[@class='btn facebook-btn w-100 m-0 mb-3']")
+	public WebElement facebookSignUpButton;
+	
+	public WebElement email;//facebook Email or Phone textbox
+	public WebElement pass;//facebook password textbox
+	public WebElement u_0_0; //facebook login button
 }
--- src/test/java/com/fourstay/tests/FS001.java
+++ src/test/java/com/fourstay/tests/FS001.java
@@ -3,6 +3,7 @@ package com.fourstay.tests;
 import org.testng.annotations.Test;
 
 import com.fourstay.pages.HomePage;
+import com.fourstay.pages.SignUpPage;
 import com.fourstay.utilities.Configuration;
 import com.fourstay.utilities.TestBaseClass;
 
@@ -14,6 +15,11 @@ public class FS001 extends TestBaseClass{
 		HomePage homePage = new HomePage(driver);
 		homePage.signUp.click();
 		System.out.println("Sign Up Page");
+		SignUpPage signUpPage = new SignUpPage(driver);
+		signUpPage.facebookSignUpButton.click();
+		signUpPage.email.sendKeys(Configuration.getProperty("facebookuser"));
+		signUpPage.pass.sendKeys(Configuration.getProperty("facebookpass"));
+		signUpPage.u_0_0.click();
 	}
 	
 }
