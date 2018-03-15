5dfc769f800a9a5dfb49f2fd14de530d8effe7cd
handan.tekin85@gmail.com <handan.tekin85@gmail.com>
2018-03-15 10:36:28 -0500

    Step 1 in Test Case FS002 is done by Handan

--- src/test/java/com/fourstay/tests/FS002.java
+++ src/test/java/com/fourstay/tests/FS002.java
@@ -1,8 +1,12 @@
 package com.fourstay.tests;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 
+import com.fourstay.pages.HomePage;
 import com.fourstay.utilities.Configuration;
 import com.fourstay.utilities.TestBaseClass;
 
@@ -11,6 +15,16 @@ public class FS002 extends TestBaseClass{
 	@Test 
 	public void Test2() {
 		driver.get(Configuration.getProperty("url"));
+		
+		//Step 1
+		HomePage homePage = new HomePage(driver);
+		// verify home page loaded(the left top corner logo is displayed)
+		assertTrue(homePage.logo.isDisplayed());
+		// verify the Current URL is "https://4stay.com/"
+		assertEquals(driver.getCurrentUrl(), "https://4stay.com/");
+		// verify the Home page title is "Room rental, roommate finder, off-campus
+		// housing, homestay | 4stay"
+		assertTrue(homePage.isAt());
 	}
 	
 }
