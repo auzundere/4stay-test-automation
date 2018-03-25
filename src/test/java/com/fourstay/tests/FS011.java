8d1eb4294cae2f31c3fdd617365490879969092d
selami3448 <selami@192.168.1.2>
2018-03-25 01:32:54 -0500

    last updated on Sunday 1:32 am by Selami

--- src/test/java/com/fourstay/tests/FS011.java
+++ src/test/java/com/fourstay/tests/FS011.java
@@ -1,5 +1,49 @@
 package com.fourstay.tests;
 
-public class FS011 {
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
+import org.testng.annotations.Test;
+
+import com.fourstay.pages.HomePage;
+import com.fourstay.pages.LoginPage;
+import com.fourstay.utilities.BrowserUtils;
+import com.fourstay.utilities.Configuration;
+import com.fourstay.utilities.TestBaseClass;
+
+
+
+public class FS011 extends TestBaseClass {
+	@Test
+	public void Test11() throws InterruptedException {
+		driver.get(Configuration.getProperty("url"));
+		LoginPage loginPage=new LoginPage(driver);
+		
+		//Step 1
+		HomePage homePage = new HomePage(driver);
+		//verify home page loaded(the left top corner logo is displayed)
+		assertTrue(homePage.logo.isDisplayed());
+		//verify the Current URL is "https://4stay.com/"
+		assertEquals(driver.getCurrentUrl(),"https://fourstay-staging.herokuapp.com/");
+		//verify the Home page title is "Room rental, roommate finder, off-campus housing, homestay | 4stay"
+		assertTrue(homePage.isAt());
+	
+		//Step 2
+		homePage.loginLink.click();
+		BrowserUtils.waitForVisibility(loginPage.user_email, 1);
+		assertTrue(loginPage.user_email.isDisplayed(),"email field was not displayed!");
+		assertTrue(loginPage.user_password.isDisplayed(),"password field was not displayed!");
+		assertTrue(loginPage.loginWithFacebook.isDisplayed(),"Login with Facebook was not displayed!");
+		assertTrue(loginPage.loginWithGoogle.isDisplayed(),"Login with Google was not displayed!");
+		
+		//Step3
+		loginPage.loginWithFacebook.click();
+		//There is a bug right here. We can't do the rest.
+		
+		
+		
+	
+	}
+	
+	
 }
--- src/test/java/com/fourstay/tests/FS017.java
+++ src/test/java/com/fourstay/tests/FS017.java
@@ -1,5 +1,41 @@
 package com.fourstay.tests;
 
-public class FS017 {
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import com.fourstay.pages.HomePage;
+import com.fourstay.pages.LoginPage;
+import com.fourstay.utilities.Configuration;
+import com.fourstay.utilities.TestBaseClass;
+
+public class FS017 extends TestBaseClass{
+	@Test 
+	public void Test017() {
+		driver.get(Configuration.getProperty("url"));
+	
+				//Step 1
+				HomePage homePage = new HomePage(driver);
+				//verify the Current URL is "https://fourstay-staging.herokuapp.com/"
+				assertEquals(driver.getCurrentUrl(),"https://fourstay-staging.herokuapp.com/");
+				//verify the Home page title is "Room rental, roommate finder, off-campus housing, homestay | 4stay"
+				assertTrue(homePage.isAt());
+
+				//Step 2
+				//Verify search input field is on the homepage
+				assertTrue(homePage.searchBox.isDisplayed());
+				
+				//Step 3
+				//Enter City Name as serach input,Select on of the option from the resulted dropdown menu
+				homePage.searchBox.sendKeys("St Louis");
+			
+				//Select dropDownMenu = new Select(driver.findElement(By.xpath("//input[@placeholder='Where are you going? (type your city, college, or metro)']")));
+				//dropDownMenu.selectByVisibleText("St. Louis, MO, USA");
+
+				
+	
+	}
 }
